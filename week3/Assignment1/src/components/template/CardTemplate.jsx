400a034c4439adb9b3788bb24cf5f77e602a6e64
Geun-Oh <kandy1002@naver.com>
2023-04-30 16:37:54 +0900

    chore: 이미지 빌드 경로 수정

--- week3/Assignment1/src/components/template/CardTemplate.jsx
+++ week3/Assignment1/src/components/template/CardTemplate.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import styled from 'styled-components';
 
-import ImgChunks from '../../assets';
+import ImgChunks from '../../utils/imgChunks';
 import { useGlobalContext } from '../../context/reducer';
 import { getRandomIndexArray } from '../../utils/getRandomIndexArray';
 import Card from '../organism/Card';
@@ -41,6 +41,7 @@ const CardTemplate = () => {
   return (
     <StyledCardWrapper>
       {randomArray.map((x, index) => {
+        // ImgChunks[`IMG_${x}`]
         return <Card imgURL={ImgChunks[`IMG_${x}`]} key={index} index={index} />;
       })}
     </StyledCardWrapper>
